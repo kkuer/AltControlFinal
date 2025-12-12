9b3ca1ebdf78217a66ee59ed221a4f65fd953037
kflores33 <159832582+kflores33@users.noreply.github.com>
2025-12-11 21:50:39 -0800

    added plugin

---
+++ ImOneOfYou/Assets/Scripts/MessageListener.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class MessageListener : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        Debug.Log("Arrived: " + msg);
+    }
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device connected" : "Device disconnected");
+    }
+}
